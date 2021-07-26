d31a67e128084bea5f93c07ab3519869b0b56397
unknown <mrchrisfarrell@yahoo.com>
2021-07-26 15:49:10 -0400

    setup complete, companyProfile started

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 
 function App() {
   return (
---
+++ src/Components/CompanyProfile/companyProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CompanyProfile = (props) => {
+
+
+    return (
+        <div className="profile-container">
+
+        </div>
+    )
+}
+
+export default CompanyProfile;
